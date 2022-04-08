bd5a8b93ecb582a6b99986bbd300172f770f54fc
EmmanuelBazan <emanuel.bazan@track.design>
2022-04-08 12:00:48 -0500

    Implementacion del servicio oneSignal

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+import Constants from "expo-constants";
+
+OneSignal.setAppId(Constants.manifest.extra.oneSignalAppId);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>One Signal</Text>
       <StatusBar style="auto" />
     </View>
   );
